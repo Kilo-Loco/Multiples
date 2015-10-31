8ea00f7bfce6da0e9d7030e05f2e1bf8eeb364ce
Kyle Lee <kiloloco@me.com>
2015-10-31 03:02:58 -0700

    Multiple Apps basics completed

--- Multiples/ViewController.swift
+++ Multiples/ViewController.swift
@@ -8,19 +8,94 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    // Primary view elements
     @IBOutlet var multiplesLogo: UIImageView!
     @IBOutlet var postit: UIImageView!
     @IBOutlet var playImage: UIButton!
+    @IBOutlet var multipleInput: UITextField!
     
+    // Secondary View elements
     @IBOutlet var sumText: UILabel!
     @IBOutlet var addImage: UIButton!
     
-    @IBAction func playButton(sender: UIButton) {
+    // Global
+    var counter: Int = 0
+    var validMultiple: Int = 0
+    var nextMultiple: Int = 1
+    
+    func showViewElements() {
+        
+        if counter % 2 == 0 {
+            
+            // Show Primary View
+            self.multiplesLogo.hidden = false
+            self.postit.hidden = false
+            self.playImage.hidden = false
+            self.multipleInput.hidden = false
+            self.sumText.hidden = true
+            self.addImage.hidden = true
+            
+            // Reset elements
+            self.multipleInput.text = ""
+            self.validMultiple = 0
+            self.nextMultiple = 1
+            
+        } else {
+            
+            // Show Secondary View
+            self.multiplesLogo.hidden = true
+            self.postit.hidden = true
+            self.playImage.hidden = true
+            self.multipleInput.hidden = true
+            self.sumText.hidden = false
+            self.addImage.hidden = false
+            
+        }
     }
     
+    // Play Button
+    @IBAction func playButton(sender: UIButton) {
+        
+        // Check input for interger value
+        if let userNumber: Int = Int(self.multipleInput.text!) {
+            
+            // Check input for valid multiple
+            if self.multipleInput.text != "" {
+                
+                self.sumText.text = "\(userNumber) + 5 = \(userNumber + 5)"
+                self.validMultiple = userNumber
+                self.counter++
+                self.showViewElements()
+            }
+            
+        } else {
+            
+            // Invalid Number Alert Controller
+            let alertController: UIAlertController = UIAlertController(title: "Invalid Number", message: "Please enter a valid multiple.", preferredStyle: .Alert)
+            
+            let alertAction: UIAlertAction = UIAlertAction(title: "OK", style: .Cancel, handler: nil)
+            
+            alertController.addAction(alertAction)
+            
+            self.presentViewController(alertController, animated: true, completion: nil)
+            
+            self.multipleInput.text = ""
+        }
+        
+    }
+    
+    // Add Button
     @IBAction func addButton(sender: AnyObject) {
+        self.nextMultiple++
+        let visibleMultiple = self.validMultiple * self.nextMultiple
+        self.sumText.text = "\(visibleMultiple) + 5 = \(visibleMultiple + 5)"
+        
+        if self.nextMultiple >= 11 {
+            counter++
+            self.showViewElements()
+        }
     }
     
     
@@ -34,6 +109,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 }
 
