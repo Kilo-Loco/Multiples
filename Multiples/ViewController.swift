73712392622129b72c713c582b2329a4ee7cfd06
Kyle Lee <kiloloco@me.com>
2015-10-31 01:26:33 -0700

    Set up all variables with storyboard elements

--- Multiples/ViewController.swift
+++ Multiples/ViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var multiplesLogo: UIImageView!
+    @IBOutlet var postit: UIImageView!
+    @IBOutlet var playImage: UIButton!
+    
+    @IBOutlet var sumText: UILabel!
+    @IBOutlet var addImage: UIButton!
+    
+    @IBAction func playButton(sender: UIButton) {
+    }
+    
+    @IBAction func addButton(sender: AnyObject) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
